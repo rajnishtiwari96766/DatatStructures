c9c9a76e868312a8cf3a509ab688787cf5b9c4db
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-07-11 13:14:25 +0530

    new commit

--- src/com/company/array_product.java
+++ src/com/company/array_product.java
@@ -1,19 +1,41 @@
+////find the product of the array except itself...
 //package com.company;
-//
 //public class array_product {
-//    static int[] arr_product(int []nums){
-//
-//        int [] ans=new int[nums.length];
-//        int i=0;
-//
-//        if(i==0){
-//            ans[i]==
+//    static int[] productExceptSelf(int []nums){
+//        int n=nums.length;
+//        int [] ans=new int[n];
+//        int product=1;
+//        for (int i=0;i<n;i++){
+//            for (int j=0;j<n;j++){
+//                if(i==j){
+//                    continue;
+//                }
+//                product*=ans[j];
+//            }
 //        }
-//        for (i=1;i< nums.length-1;i++){
-//
-//        }
+//        return  ans;
 //    }
 //    public static void main(String[] args) {
 //
+//    }
+//}
+
+//-----------------------------------------------------------//
+//class Solution {
+//    public int[] productExceptSelf(int[] nums) {
+//        int n=nums.length;
+//        int ans[]=new int [n];
+//        Arrays.fill(ans,1);
+//        int curr=1;
+//        for(int i=0;i<n;i++){
+//            ans[i]*=curr;
+//            curr*=nums[i];
+//        }
+//        curr=1;
+//        for(int i=n-1;i>=0;i--){
+//            ans[i]*=curr;
+//            curr*=nums[i];
+//        }
+//        return ans;
 //    }
 //}
