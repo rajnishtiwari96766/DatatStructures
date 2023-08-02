f97b34b87769c0f6cc37ea76cc2533dbef865325
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-08-03 02:19:07 +0530

    new commit

--- src/com/company/is_subseq.java
+++ src/com/company/is_subseq.java
@@ -1,16 +1,42 @@
 //check if the subseq is present or not...
+//package com.company;
+//public class is_subseq {
+//    static boolean subseq(String s,String t){
+//        boolean seq_present=false;
+//        for(int i=0;i<t.length();i++){
+//            if(!s.contains(t.charAt(i))){
+//                t.remove();
+//            }
+//        }
+//
+//        if(s==t){
+//            seq_present=true;
+//        }
+//        return seq_present;
+//    }
+//    public static void main(String[] args) {
+//
+//    }
+//}
+
+
 package com.company;
-
 public class is_subseq {
-    static boolean issubseq(String s,String t){
+    static boolean subseq(String s,String t){
         int i=0;
-       char [] first= s.toCharArray();
-       char [] sec= t.toCharArray();
+        boolean seq_present=false;
+//        i=s.charAt(i);
 
-
-        for (int j=0;j<sec.length;j++){
-
+        for(int j=0;j<t.length();j++){
+                if(t.charAt(j)==s.charAt(i)){
+                    i++;
+                }
         }
+
+        if(i==s.length()){
+            seq_present=true;
+        }
+        return seq_present;
     }
     public static void main(String[] args) {
 
